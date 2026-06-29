e0978e8973f776a92740068603509c29d82ce594
bacor <bacoranaawuso@gmail.com>
2026-06-29 15:35:17 +0300

    fix vite static path

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
-from django.conf import settings
-from django.conf.urls.static import static
-
 urlpatterns = [
     path("admin/", admin.site.urls),
 
@@ -13,11 +10,12 @@ urlpatterns = [
     path("api/events/", include("events.urls")),
 ]
 
-# React SPA fallback (IMPORTANT: last)
+# React routes only (exclude api + static + media)
 urlpatterns += [
-    re_path(r'^(?!api/).*$', TemplateView.as_view(template_name="index.html")),
+    re_path(
+        r'^(?!api/|static/|media/).*$',
+        TemplateView.as_view(template_name="index.html")
+    )
 ]
-
-# MEDIA files (ONLY)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
